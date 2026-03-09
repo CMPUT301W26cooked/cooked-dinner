81cfa84d36fe611df7fc8bc48317ec7f665d725c
Luke Forster <lukejforster175@gmail.com>
2026-03-08 21:34:39 -0600

    Feat: Initial Event widget

--- src/app/src/main/java/com/eventwise/MainActivity.java
+++ src/app/src/main/java/com/eventwise/MainActivity.java
@@ -1,6 +1,9 @@
 package com.eventwise;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +11,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * This is the MainActivity class for the EventWise app. It acts as a starting point for all profile
+ * types and will likely eventually become the login page for the app.
+ * @author Luke Forster
+ * @version 1.0
+ * @since 2026-03-03
+ */
 
+
+ // TODO Implement organizer landing page and admin landing page
+
+public class MainActivity extends AppCompatActivity {
+    private LinearLayout loginButtonsContainer;
+    private View fragmentContainer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +35,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        loginButtonsContainer = findViewById(R.id.login_buttons_container);
+        fragmentContainer = findViewById(R.id.fragment_container);
+
+        Button entrantButton = findViewById(R.id.entrant_button);
+        Button organizerButton = findViewById(R.id.organizer_button);
+        Button adminButton = findViewById(R.id.admin_button);
+
+        entrantButton.setOnClickListener(v -> {
+            loginButtonsContainer.setVisibility(View.GONE);
+            fragmentContainer.setVisibility(View.VISIBLE);
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, new EntrantEventsCommunityFragment())
+                    .commit();
+        });
+        organizerButton.setOnClickListener(v -> {
+            // TODO: link organizer flow later
+        });
+
+        adminButton.setOnClickListener(v -> {
+            // TODO: link admin flow later
+        });
+
     }
 }
