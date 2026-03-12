92e6f90f92177665d540b98f96f4b90fce604f43
Rana Sunjog Singh Thind <71564731+sunjogthind@users.noreply.github.com>
2026-03-12 16:02:38 -0600

    add image gallery adapter

--- src/app/src/main/java/com/eventwise/ImageGalleryAdapter.java
+++ src/app/src/main/java/com/eventwise/ImageGalleryAdapter.java
@@ -21,6 +21,11 @@ public class ImageGalleryAdapter extends RecyclerView.Adapter<ImageGalleryAdapte
     private static ArrayList<ImageListItem> galleryList;
     private static Context context;
     private static ImageGalleryAdapter galleryAdapterInstance;
+    private static OnImageClickListener listener;
+
+    public interface OnImageClickListener {
+        void onImageClick(int position, ImageListItem item);
+    }
 
     /**
      * ImageGalleryAdapter constructor.
@@ -35,24 +40,66 @@ public class ImageGalleryAdapter extends RecyclerView.Adapter<ImageGalleryAdapte
 
     }
 
+  
+    public void setOnImageClickListener(OnImageClickListener listener) {
+        this.listener = listener;
+    }
+
+
     @NonNull
     @Override
     public ImageGalleryAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        return null;
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.image_cell_layout, viewGroup, false);
+        return new ViewHolder(view);
     }
 
+    
     @Override
     public void onBindViewHolder(ImageGalleryAdapter.ViewHolder viewHolder, int i) {
+        viewHolder.image.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        viewHolder.image.setImageBitmap(galleryList.get(i).getImage());
     }
 
+    // Returns the number of items in the gallery
     @Override
     public int getItemCount() {
-        return 0;
+        return galleryList.size();
     }
 
+  //Remove an image from gallery.
+    public void removeImage(ImageListItem imageItem){
+        galleryList.remove(imageItem);
+        notifyDataSetChanged();
+    }
+
+    // Creates the ViewHolder for iteration through images
     public class ViewHolder extends RecyclerView.ViewHolder{
+        private ImageView image;
         public ViewHolder(View view) {
             super(view);
+
+            image = view.findViewById(R.id.image);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position = getBindingAdapterPosition();
+                    ImageListItem item = galleryList.get(position);
+                    if (listener != null) {
+                        listener.onImageClick(position, item);
+                    }
+                }
+            });
+
         }
     }
+
+ 
+    public static ImageGalleryAdapter getInstance() {
+        if (galleryAdapterInstance == null) {
+            galleryAdapterInstance = new ImageGalleryAdapter(context,galleryList);
+        }
+        return galleryAdapterInstance;
+    }
+
+
 }
