acb4eb98e2c2e7c39b5d9af0cc3ad400e31a239b
Luke Forster <lukejforster175@gmail.com>
2026-04-06 16:45:40 -0600

    oops fixed

--- src/app/build.gradle.kts
+++ src/app/build.gradle.kts
@@ -72,7 +72,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.robolectric:robolectric:4.14.1")
     testImplementation("androidx.test:core:1.6.1")
-    testImplementation("androidx.fragment:fragment-testing:1.8.6")
     debugImplementation("androidx.fragment:fragment-testing:1.6.2")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.robolectric:robolectric:4.14.1")
